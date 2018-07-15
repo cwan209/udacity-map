80ee48725b10a772b30287f10473db8f8899e32b
Chenlu Wang <lukechenluwang@gmail.com>
2018-07-15 18:14:12 +1000

    added filter input text field

--- src/view/LocationList.js
+++ src/view/LocationList.js
@@ -2,50 +2,48 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
-import { List, ListItem, ListItemText} from '@material-ui/core';
+import { List, ListItem, ListItemText, Input, FormControl, InputLabel, InputAdornment, TextField} from '@material-ui/core';
 
 class LocationList extends Component {
     constructor(props) {
         super(props);
-    }
 
-    componentWillMount() {
-
+        this.state = {
+            keyword: ''
+        }
     }
 
     componentDidMount() {
 
     }
 
-    componentWillReceiveProps(nextProps) {
-
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-
-    }
-
-    componentWillUnmount() {
-
-    }
-
     render() {
         const { locations } = this.props;
+        const { keyword } = this.state;
 
         return (
             <div>
+                <FormControl fullWidth className="Filter">
+                    <TextField
+                        id="select-currency-native"
+                        label="Native select"
+                        // className={classes.textField}
+                        value={keyword}
+                        onChange={event => this.setState({ keyword: event.target.value})}
+                        // SelectProps={{
+                        //     native: true,
+                        //     MenuProps: {
+                        //         className: classes.menu,
+                        //     },
+                        // }}
+                        helperText="Please filter"
+                        margin="normal"
+                    />
+                </FormControl>
                 <List component="nav">
                 {
-                    locations.map(location =>
-                        <ListItem button>
+                    locations.map((location, index) =>
+                        <ListItem button key={index}>
                             <ListItemText primary={location.title} />
                         </ListItem>
                     )
